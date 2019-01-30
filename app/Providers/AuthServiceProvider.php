99bf462d59dd59da30ae4f333a056c3a1e34f0b4
vcau <vcau@hevaweb.com>
2019-01-30 10:14:57 +0100

    add Invoice Policy !!! no work !!! todo fix it

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Nova\Invoice;
+use App\Policies\InvoicePolicy;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
@@ -13,7 +15,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        'App\Model' => 'App\Policies\ModelPolicy',
+        Invoice::class => InvoicePolicy::class,
     ];
 
     /**
